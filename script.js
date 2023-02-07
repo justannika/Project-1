2f2270a4d22350788acbb294f00cd549ea12cb1e
Annika Schrenk <schrenkannika@gmail.com>
2023-02-07 21:34:42 +0000

    Date Hobby Page

--- script.js
+++ script.js
@@ -11,5 +11,12 @@ function formatTime(date) {
 function formatDate(date) {
     const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    return `${DAYS[date.getDay()]}, ${MONTHS[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`;
+    return `${DAYS[date.getDay()]} ${MONTHS[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
 }
+
+setInterval(() => {
+    const now = new Date ();
+
+    timeElement.textContent = formatTime (now);
+    dateElement.textContent = formatDate (now);
+}, 200);
